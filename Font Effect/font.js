45861a2f83362734b9349c643411bba0e68da678
mmmG <a331998513@gmail.com>
2021-03-25 14:48:44 +0100

    2

--- Font Effect/font.js
+++ Font Effect/font.js
@@ -1,5 +1,4 @@
 //setup
-
 const canvas = document.querySelector('canvas');
 const ctx = canvas.getContext('2d');
 canvas.width = window.innerWidth;
@@ -12,7 +11,7 @@ let adjustY = 0;
 const mouse = {
     x: null,
     y: null, 
-    radius: 70
+    radius: 50
 }
 window.addEventListener('mousemove', function(event){
     mouse.x = event.x;  //default X coordinate
